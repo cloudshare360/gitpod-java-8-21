f84f4efa08e37eb0ee06d4cb884e6c81db46d780
cloudshare360 <cloud.share360@gmail.com>
2025-06-02 15:44:07 +0000

    adding  method references
    Signed-off-by: cloudshare360 <cloud.share360@gmail.com>

---
+++ java-8-educative/03-Stream-API/04-Method-References/MethodReferencesApp/src/com/staticmethodreference/StreamDemo.java
@@ -0,0 +1,30 @@
+package com.staticmethodreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamDemo {
+
+    public static int getLength(String str){
+        return str.length();
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("done");
+        list.add("word");
+        list.add("practice");
+        list.add("fake");
+
+        // Code without using method reference.
+        list.stream()
+                .mapToInt(str -> StreamDemo.getLength(str))
+                .forEach(System.out::println);
+
+        // Code with method reference.
+        list.stream()
+                .mapToInt(StreamDemo::getLength)
+                .forEach(System.out::println);
+
+    }
+}
